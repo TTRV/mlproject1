19bd18dc031bfa54a7e7ce70822fdb9dfcf96dd2
Basava Dinesh <basavadinesh7@gmail.com>
2024-04-19 19:30:20 +0530

    Author name updated with a comma in setup.py
    * The author name in the file setup.py was updated from 'TTRV' to 'TTRV,'.
    * No changes were made to the functionality of the code

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setup_tools import find_packages,setup
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'TTRV,'
+    author_email = 'basavadinesh7@gmail.com',
+    packages = find_packages(),
+    install_requires = ['numpy','pandas','seaborn']
+
+
+
+
+)
